306b32af68043f8fb0f535adfb658d7b3bdb3a7f
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-11-03 14:01:15 +0100

    v0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyobs-asi',
-    version='0.13',
+    version='0.14',
     description='pyobs component for ASI cameras',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',
